7e43f0b7607c0619f039972e460167dcc12ce9a9
maxwatchmaker <m.c.watchmaker@wustl.edu>
2026-03-24 12:35:41 -0500

    transfer money method complete

--- src/test/BankAccountTest.java
+++ src/test/BankAccountTest.java
@@ -1,29 +1,66 @@
 package test;
-
 import main.BankAccount;
-
+import main.Customer;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BankAccountTest {
 
-    @Test
-    public void testDeposit() {
-        BankAccount testAccount = new BankAccount();
-        testAccount.deposit(50);
-        assertEquals(50, testAccount.getBalance(), 0.01);
+    private BankAccount sender;
+    private BankAccount receiver;
+
+    @BeforeEach
+    public void setUp() {
+        sender = new BankAccount();
+        receiver = new BankAccount();
     }
 
     @Test
-    public void testInvalidDeposit() {
-        BankAccount testAccount = new BankAccount();
-        try {
-            testAccount.deposit(-50);
-            fail();
-        } catch (IllegalArgumentException e) {
-            //do nothing, test passes
-        }
+    public void testTransferValidAmount() {
+        sender.deposit(100.0);
+        sender.transfer(receiver, 40.0);
+
+        assertEquals(60.0, sender.getBalance(), 0.0001);
+        assertEquals(40.0, receiver.getBalance(), 0.0001);
+    }
+
+    @Test
+    public void testTransferZeroAmountThrowsException() {
+        sender.deposit(100.0);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            sender.transfer(receiver, 0.0);
+        });
+    }
+
+    @Test
+    public void testTransferNegativeAmountThrowsException() {
+        sender.deposit(100.0);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            sender.transfer(receiver, -25.0);
+        });
+    }
+
+    @Test
+    public void testTransferMoreThanBalanceThrowsException() {
+        sender.deposit(50.0);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            sender.transfer(receiver, 75.0);
+        });
+    }
+
+    @Test
+    public void testTransferExactBalance() {
+        sender.deposit(80.0);
+        sender.transfer(receiver, 80.0);
+
+        assertEquals(0.0, sender.getBalance(), 0.0001);
+        assertEquals(80.0, receiver.getBalance(), 0.0001);
     }
 }
